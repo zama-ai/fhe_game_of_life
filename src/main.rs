3d9a0cd42e74a3781929f8a1b72b7dc823dee5c1
aquint-zama <alexandre.quint@zama.ai>
2022-07-21 18:00:29 +0200

    Use default FheUint3

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,45 @@
 use concrete::prelude::*;
-use concrete::{generate_keys, set_server_key, ConfigBuilder, FheBool};
-use std::ops::AddAssign;
+use concrete::{
+    set_server_key,ConfigBuilder, FheUint3, KeyCacher,
+};
 
-#[derive(Clone)]
-struct Accumulator(FheBool, FheBool, FheBool);
-
-impl From<(FheBool, FheBool, FheBool)> for Accumulator {
-    fn from(inner: (FheBool, FheBool, FheBool)) -> Self {
-        Self(inner.0, inner.1, inner.2)
-    }
-}
-
-impl AddAssign<&FheBool> for Accumulator {
-    // ^ -> xor
-    // & -> and
-    fn add_assign(&mut self, rhs: &FheBool) {
-        let first_carry = rhs & &self.0;
-        self.0 = &self.0 ^ rhs;
-
-        let second_cary = &first_carry & &self.1;
-        self.1 = &self.1 ^ first_carry;
-
-        self.2 = &self.2 ^ second_cary;
-    }
-}
+const KEY_PATH: &str = "keys.bin";
 
 /// Rules are
 ///
 /// a live cell will survive if it has 2 or 3 neighbours alive
 /// a dead cell will birth if it has 3 neighbours alive
-fn is_alive(cell: &FheBool, neighbours: &[&FheBool], mut accumulator: Accumulator) -> FheBool {
-    for neighbour in neighbours {
-        accumulator += *neighbour;
+fn is_alive(cell: &FheUint3, neighbours: &[&FheUint3]) -> FheUint3 {
+    let mut num_neighbours_alive = FheUint3::try_encrypt_trivial(0).unwrap();
+    for n in neighbours {
+        num_neighbours_alive += *n;
     }
+    // The above could be written as:
+    // let num_neighbours_alive: FheUint3 = neighbours.into_iter().copied().sum();
 
-    // check if sum is equal to 2 or 3
-    let sum_is_2_or_3 = !accumulator.2 & accumulator.1;
-    let sum_is_3 = &sum_is_2_or_3 & accumulator.0;
-
-    sum_is_3 | cell & sum_is_2_or_3
+    num_neighbours_alive.eq(3) | (cell & num_neighbours_alive.eq(2))
 }
 
 struct Board {
     dimensions: (usize, usize),
-    states: Vec<FheBool>,
-    clean_accumulator: Accumulator,
+    states: Vec<FheUint3>,
+    new_states: Vec<FheUint3>,
 }
 
 impl Board {
-    pub fn new(n_cols: usize, states: Vec<FheBool>, zeros: (FheBool, FheBool, FheBool)) -> Self {
+    pub fn new(n_cols: usize, states: Vec<FheUint3>) -> Self {
         let n_rows = states.len() / n_cols;
+        let n_elem = states.len();
 
         Self {
             dimensions: (n_rows, n_cols),
             states,
-            clean_accumulator: Accumulator::from(zeros),
+            new_states: Vec::with_capacity(n_elem),
         }
     }
 
     pub fn update(&mut self) {
-        let mut new_states = Vec::<FheBool>::with_capacity(self.states.len());
+        self.new_states.clear();
 
         let nx = self.dimensions.0;
         let ny = self.dimensions.1;
@@ -81,16 +61,15 @@ impl Board {
                 let n8 = &self.states[ip * ny + jp];
 
                 // see if the cell is alive of dead
-                new_states.push(is_alive(
+                self.new_states.push(is_alive(
                     &self.states[i * ny + j],
-                    &vec![n1, n2, n3, n4, n5, n6, n7, n8],
-                    self.clean_accumulator.clone(),
+                    &[n1, n2, n3, n4, n5, n6, n7, n8],
                 ));
             }
         }
 
         // update the board
-        self.states = new_states;
+        std::mem::swap(&mut self.new_states, &mut self.states);
     }
 }
 
@@ -100,51 +79,51 @@ fn main() {
 
     let (n_rows, n_cols): (usize, usize) = (6, 6);
 
-    let config = ConfigBuilder::all_disabled().enable_default_bool().build();
-
-    let (client_key, server_key) = generate_keys(config);
-
-    let zeros = (
-        FheBool::encrypt(false, &client_key),
-        FheBool::encrypt(false, &client_key),
-        FheBool::encrypt(false, &client_key),
-    );
+    let config = ConfigBuilder::all_disabled().enable_default_uint3().build();
+    let (client_key, server_key) = KeyCacher::new(
+        KEY_PATH,
+        config,
+        bincode::serialize_into,
+        bincode::deserialize_from,
+    )
+    .get();
 
     // initial configuration
     #[rustfmt::skip]
     let states = vec![
-        true, false, false, false, false, false,
-        false, true, true, false, false, false,
-        true, true, false, false, false, false,
-        false, false, false, false, false, false,
-        false, false, false, false, false, false,
-        false, false, false, false, false, false,
+        1, 0, 0, 0, 0, 0,
+        0, 1, 1, 0, 0, 0,
+        1, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
     ];
 
     // encrypt the initial configuration
     let states: Vec<_> = states
         .into_iter()
-        .map(|x| FheBool::encrypt(x, &client_key))
+        .map(|x| FheUint3::try_encrypt(x, &client_key).unwrap())
         .collect();
 
     set_server_key(server_key);
 
-    let mut board = Board::new(n_cols, states, zeros);
+    let mut board = Board::new(n_cols, states);
 
     let mut count = 0;
     loop {
-        println!("iter: {}", count);
+        print!("iter: {}", count);
         // show the board
         for i in 0..n_rows {
             println!();
             for j in 0..n_rows {
-                if (&board.states[i * n_cols + j]).decrypt(&client_key) {
+                if (&board.states[i * n_cols + j]).decrypt(&client_key) != 0 {
                     print!("█");
                 } else {
                     print!("░");
                 }
             }
         }
+        println!();
 
         // increase the time step
         board.update();
@@ -155,43 +134,4 @@ fn main() {
     }
 
     println!("Elapsed time: {:.2?}", before.elapsed());
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::Accumulator;
-    use concrete::prelude::*;
-    use concrete::{generate_keys, ConfigBuilder, FheBool};
-
-    fn decrypt_acc(acc: &Accumulator, keys: &mut KeyChain) -> (FheBool, FheBool, FheBool) {
-        (
-            acc.2.to_FheBool(keys),
-            acc.1.to_FheBool(keys),
-            acc.0.to_FheBool(keys),
-        )
-    }
-
-    #[test]
-    fn test_accumulator() {
-        let config = ConfigBuilder::all_disabled().enable_default_bool().build();
-
-        let (client_key, server_key) = generate_keys(config);
-
-        let zeros = (
-            FheBool::encrypt(false, &client_key),
-            FheBool::encrypt(false, &client_key),
-            FheBool::encrypt(false, &client_key),
-        );
-
-        let mut accumulator = Accumulator::from(zeros);
-        let ftrue = FheBool::encrypt(true, &mut keys);
-        let ffalse = FheBool::encrypt(false, &mut keys);
-
-        let bits = decrypt_acc(&accumulator, &mut keys);
-        assert_eq!(bits, (false, false, false));
-
-        accumulator += &ftrue;
-        let bits = decrypt_acc(&accumulator, &mut keys);
-        assert_eq!(bits, (false, false, true));
-    }
 }
